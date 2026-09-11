fba1417ce1f47d4e2c5ccf1f9d74c993743982b7
Alexander Mills <alexander.d.mills@gmail.com>
2026-09-11 15:39:24 -0500

    wip: preserve uncommitted working tree before sync (20260911)
    Snapshot of tracked edits and untracked (non-ignored) files, taken with a temporary index;
    HEAD, index and working tree were not modified. Secrets-like and >20MB files excluded.

--- target/release/deps/wasm_bindgen_macro-c8a044bda9545358.d
+++
@@ -1,5 +0,0 @@
-/Users/maca5/codes/unreal-unity-poc/rust-unity-unreal-poc/wasm/target/release/deps/wasm_bindgen_macro-c8a044bda9545358.d: /Users/maca5/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/wasm-bindgen-macro-0.2.122/src/lib.rs
-
-/Users/maca5/codes/unreal-unity-poc/rust-unity-unreal-poc/wasm/target/release/deps/libwasm_bindgen_macro-c8a044bda9545358.dylib: /Users/maca5/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/wasm-bindgen-macro-0.2.122/src/lib.rs
-
-/Users/maca5/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/wasm-bindgen-macro-0.2.122/src/lib.rs:
--- target/wasm32-unknown-unknown/release/deps/cfg_if-87412499c4f36ede.d
+++
@@ -1,7 +0,0 @@
-/Users/maca5/codes/unreal-unity-poc/rust-unity-unreal-poc/wasm/target/wasm32-unknown-unknown/release/deps/cfg_if-87412499c4f36ede.d: /Users/maca5/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.4/src/lib.rs
-
-/Users/maca5/codes/unreal-unity-poc/rust-unity-unreal-poc/wasm/target/wasm32-unknown-unknown/release/deps/libcfg_if-87412499c4f36ede.rlib: /Users/maca5/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.4/src/lib.rs
-
-/Users/maca5/codes/unreal-unity-poc/rust-unity-unreal-poc/wasm/target/wasm32-unknown-unknown/release/deps/libcfg_if-87412499c4f36ede.rmeta: /Users/maca5/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.4/src/lib.rs
-
-/Users/maca5/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.4/src/lib.rs:
